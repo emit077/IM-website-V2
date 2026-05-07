de68302b5b554baaf37d680a11d2b303922d86a0
amit.sahu <emitsahu077@gmail.com>
2026-05-07 14:48:18 +0530

    Deploy static site 2026-05-07T09:18Z

---
+++ _next/static/ffqjttkJvG9oF0uX0vPhj/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/IM-website-V2//_next/:path+",
+        "destination": "/IM-website-V2/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
